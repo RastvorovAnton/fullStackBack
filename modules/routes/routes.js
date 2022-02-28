f7f858efc61c5b8b2afec1dd01644f44ff8bce44
Anton Rastvorov <antonrostvorov.exceedteam@gmail.com>
2022-02-28 16:10:23 +0300

    add opportunity to delete ticket

--- modules/routes/routes.js
+++ modules/routes/routes.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const {
   addNewTicket,
   getAllTickets,
+  deleteTicket,
   allUserSpending,
 } = require("../controllers/ticket.controller");
 
 router.get("/allTickets", getAllTickets);
 router.get("/allUserSpending", allUserSpending);
 router.post("/newTicket", addNewTicket);
+router.delete("/deleteTicket", deleteTicket);
 
 module.exports = router;

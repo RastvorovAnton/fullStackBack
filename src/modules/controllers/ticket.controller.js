3e4d91c3e24f49f8421f8eed7dbcee5b65055b10
Anton Rastvorov <antonrostvorov.exceedteam@gmail.com>
2022-03-01 11:17:59 +0300

    fix bags

--- src/modules/controllers/ticket.controller.js
+++ src/modules/controllers/ticket.controller.js
@@ -2,19 +2,13 @@ const Ticket = require("../../db/models/ticket/index");
 
 module.exports.getAllTickets = (req, res, next) => {
 	Ticket.find().then((result) => {
-		return res.send({ data: result });
+		res.send({ data: result });
 	})
-		.catch((err) => {
-			return res.status(422).send(err);
-		});
+		.catch((err) => { res.status(422).send("Error! Cannot find tasks") });
 };
 
 module.exports.allUserSpending = (req, res, next) => {
 	Ticket.aggregate([{ $group: { _id: null, total: { $sum: "$cost" } } }])
-		.then((result) => {
-			return res.send(result.length ? result[0] : { total: 0 });
-		})
-		.catch((err) => {
-			return res.status(422).send(err);
-		});
+		.then((result) => { res.send(result.length ? result[0] : { total: 0 }) })
+		.catch((err) => { res.status(422).send("Error! Cannot find all spending") });
 };
